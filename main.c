bb469e8eb5419b83dd8780137fb21cc640a5ae4b
haimgoldfisher <haimngoldfisher@gmail.com>
2022-01-11 00:07:54 +0200

    last

--- main.c
+++ main.c
@@ -6,8 +6,8 @@ int main()
     char curr_char;
     struct Graph g = {0, 0, NULL, NULL};
     struct Graph *graph_ptr = &g;
-    scanf(" %c", &curr_char);
-    while (curr_char != EOF || curr_char != '\n')
+    curr_char = getchar();
+    while (curr_char != EOF && curr_char != '\n')
     {
         if (curr_char == 'A') // curr graph removing & new graph building
         {
@@ -27,14 +27,14 @@ int main()
             {
                 connect(id_to_add, dest, w, graph_ptr);
             }
-            scanf(" %c", &curr_char);
+            curr_char = getchar();
         }
         else if (curr_char == 'D') // node removing (+ relevant edges)
         {
             int id_to_remove;
             scanf(" %d",&id_to_remove);
             remove_node(id_to_remove ,graph_ptr);
-            scanf(" %c", &curr_char);
+            curr_char = getchar();
         }
         else if (curr_char == 'S') // Shortest Path
         {
@@ -45,24 +45,22 @@ int main()
             if (res == 0)
                 res = -1;
             printf("Dijsktra shortest path: %d \n", res);
-            scanf(" %c", &curr_char);
+            curr_char = getchar();
         }
         else if (curr_char == 'T') // TSP - Traveling Salesman Problem
         {
             int ans = -1;
             ans = TSP(graph_ptr, &curr_char);
             printf("TSP shortest path: %d \n", ans);
-            scanf(" %c", &curr_char);
+            curr_char = getchar();
         }
         else if (curr_char == 'P') // Printer for debug
         {
             print_graph(graph_ptr);
-            scanf(" %c", &curr_char);
+            curr_char = getchar();
         }
-        else //EXIT
-        {
-            break;
-        }
+        else if (curr_char == ' ')
+            curr_char = getchar();
     }
     free_graph(graph_ptr);
     return 0;
